3c31ca36cfb5483d5b45582cdf8f50ff73503081
MistUnky <misterunknown128@gmail.com>
2023-04-22 23:13:57 -0400

    torches should not cause damage

---
+++ mods/antitorch/init.lua
@@ -0,0 +1,3 @@
+minetest.override_item("default:torch", {damage_per_second = 0})
+minetest.override_item("default:torch_wall", {damage_per_second = 0})
+minetest.override_item("default:torch_ceiling", {damage_per_second = 0})
